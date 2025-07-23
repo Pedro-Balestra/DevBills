955e58e56f1ffc670e1f674b6b38a7b3366e66c1
Pedro Balestra <pedro.balestra@outlook.com>
2025-07-23 17:56:30 -0300

    Aula 18 - Criando a Tela de Dashboard

--- interface/src/pages/Dashboard.tsx
+++ interface/src/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from "react"
-import { api } from "../services/api"
+import { useEffect, useState } from "react";
+import { MonthyearSelect } from "../components/MonthyearSelect";
+import { api } from "../services/api";
 
 export const Dashboard = () => {
+    const currentDate = new Date();
+    const [year, setYear] = useState<number>(currentDate.getFullYear());
+    const [month, setMonth] = useState(currentDate.getMonth() + 1);
 
     useEffect(() => {
         async function getTransactions() {
@@ -9,11 +13,14 @@ export const Dashboard = () => {
             console.log(responde);
         }
         getTransactions();
-    },[])
+    }, [])
 
     return (
-        <div>
-            <h1>Ola</h1>
+        <div className="container-app py-6">
+            <div className="flex felx-col md:flex-row justify-between items-start md:items-center mb-6">
+                <h1 className="text-2xl font-bold mb-4 md:mb-0">Ola</h1>
+                <MonthyearSelect month={month} year={year} onMonthChange={setMonth} onYearChange={setYear} />
+            </div>
         </div>
     )
 }
