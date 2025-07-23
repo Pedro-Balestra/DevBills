475be6fd2ffcc8ecc65197afe82c7b9d8ec6035d
Pedro Balestra <pedro.balestra@outlook.com>
2025-07-23 14:39:37 -0300

    Aula 15 - Obtendo transações do usuário

--- interface/src/pages/Dashboard.tsx
+++ interface/src/pages/Dashboard.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react"
+import { api } from "../services/api"
+
 export const Dashboard = () => {
+
+    useEffect(() => {
+        async function getTransactions() {
+            const responde = api.get('/transactions')
+            console.log(responde);
+        }
+        getTransactions();
+    },[])
+
     return (
         <div>
             <h1>Ola</h1>
