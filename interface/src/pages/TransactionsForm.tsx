a659853829f1f17012ea7e4d6ceeb0c103fcdd55
Pedro Balestra <pedro.balestra@outlook.com>
2025-07-25 13:50:00 -0300

    Aula 6 - Adicionando Tipo de Transação

--- interface/src/pages/TransactionsForm.tsx
+++ interface/src/pages/TransactionsForm.tsx
@@ -1,7 +1,61 @@
-export const TransactionsForm =()=>{
-    return(
+import { useEffect, useId, useState } from "react";
+import { Card } from "../components/Card";
+import { TransactionTypeSelector } from "../components/TransactionTypeSelector";
+import { getCategories } from "../services/category.service";
+import type { Category } from "../types/category";
+import { TransactionType } from "../types/transactions";
+
+interface FormData {
+    description: string;
+    amount: number;
+    date: string;
+    categoryId: string;
+    type: TransactionType
+}
+
+const initialFormData = {
+    description: "",
+    amount: 0,
+    date: "",
+    categoryId: "",
+    type: TransactionType.EXPENSE,
+}
+
+export const TransactionsForm = () => {
+
+    const [categories, setCategories] = useState<Category[]>([])
+    const [formData, setFormData] = useState<FormData>(initialFormData)
+    const formId = useId()
+    const handleSubmit = () => { }
+
+    const handleTransactionType = (itemType: TransactionType): void => {
+        setFormData((prev) => ({ ...prev, type: itemType }))
+    }
+
+    useEffect(() => {
+        const fetchCategories = async (): Promise<void> => {
+            const response = await getCategories()
+        }
+        fetchCategories()
+    }, [])
+
+    return (
         <div>
-            TransactionsForm
+            <div>
+                <h1>Nova Transação</h1>
+                <Card>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor={formId}>Tipo de Despesa</label>
+                            <TransactionTypeSelector
+                                id={formId}
+                                value={formData.type}
+                                onChange={handleTransactionType}
+                            />
+                        </div>
+                    </form>
+                </Card>
+            </div>
         </div>
     )
 }
